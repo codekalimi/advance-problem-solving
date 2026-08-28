3efa9d72c00e4321b57d8c59c799771aa31a600d
Aamir Kalimi <codekalimi@gmail.com>
2026-08-29 02:17:27 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        words = {}
+        for ch in s:
+            if ch in words:
+                words[ch] += 1
+            else:
+                words[ch] = 1
+        for ch in t:
+            if ch in words and words[ch] >0:
+                words[ch] -= 1
+            else:
+                return False
+        return True
